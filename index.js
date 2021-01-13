9ae7a23bf2a0c04ed193f325c8d3cc9e277a65fd
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-01-12 22:12:22 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Importando a biblioteca de API do Telegram. 
 const TelegramBot = require('node-telegram-bot-api');
-
 const dialogflow = require('./dialogflow');
+const youtube = require('./youtube');
 
 // Token recebido pelo @botfather.
 const token = '1558287466:AAEEL0WNZ9GTmy362iYZRnyA0BWoaTbnMNU';
@@ -10,18 +10,22 @@ const token = '1558287466:AAEEL0WNZ9GTmy362iYZRnyA0BWoaTbnMNU';
 const bot = new TelegramBot(token, { polling: true });
 
 // Escuta mensagens enviadas pelos usuários.
-bot.on('message', async function (msg) {
-        
+bot.on('message', async (msg) => {
+
     // ID do chat do usuário.
     const chatId = msg.chat.id;
-    console.log(msg.text);
-    
+
+    // Resposta do dialogflow.
     const dfResponse = await dialogflow.sendMessage(chatId.toString(), msg.text);
    
-    if (dfResponse.intent === 'Treino especifico') {
-        
+    // Texto a partir da resposta do dialogflow.
+    let responseText = dfResponse.text;
+
+    // Verifica a intenção a partir da resposta do dialogflow.
+    if (dfResponse.intent === 'Treino específico') {
+        responseText = await youtube.searchVideoURL(responseText, dfResponse.fields.corpo.stringValue);
     }
 
     // Envio da mensagem para o usuário do Telegram.
-    bot.sendMessage(chatId, dfResponse.text);
+    bot.sendMessage(chatId, responseText);
 });
--- youtube.js
+++ youtube.js
@@ -8,10 +8,16 @@ function searchVideoURL(message, queryText) {
     return new Promise((resolve, reject) => {
         youtube.search(`Exercio em casa para bíceps ${queryText}`, 2, function(error, result) {
             if(!error) {
-                console.log(JSON.stringify(result, null, 2));
+                // Gerando um array de videoIds.
+                const videoIds = result.items.map((item) => item.id.videoId).filter(item => item);
+                const youtubeLinks = videoIds.map(videoId => `https://www.youtube.com/watch?v=${videoId}`);
+                resolve(`${message} ${youtubeLinks}`);
             } else {
-                console.log('Deu erro!');
+                reject();
             }
         });
     });
 }
+
+// Exportando a função de busca de vídeos.
+module.exports.searchVideoURL = searchVideoURL;
